f04a07ca77680aeb0d6b3579e9ad4f3051089052
Tavva-Bhoomika <bhoomikatavva03@gmail.com>
2024-09-16 18:22:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,14 +52,7 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
-resource "aws_s3_bucket_public_access_block" "my_bucket_access_block" {
-  bucket = aws_s3_bucket.my_bucket.id
 
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-}
 
 
 data "aws_s3_bucket" "existing_website_bucket1" {
